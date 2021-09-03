85ccd368629a375814143236110d9d83ace58711
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2021-09-03 10:50:35 -0300

    Utilizando Spring Security para Segurança

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$bFNkLzI2np78/My5sCppLuharD5retL9jKwgTh2ksxcLlHXcoof2e');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
